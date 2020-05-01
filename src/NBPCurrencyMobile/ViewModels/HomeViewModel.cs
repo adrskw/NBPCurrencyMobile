78dcbd8484d5426b2abd9b0c1a24d2c35fbb0ffc
adrskw <48623654+adrskw@users.noreply.github.com>
2020-05-01 22:57:40 +0200

    Implement ItemTapped event to home page ListView

---
+++ src/NBPCurrencyMobile/ViewModels/HomeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using NBPCurrencyMobile.Views;
+using Xamarin.Forms;
+
+namespace NBPCurrencyMobile.ViewModels
+{
+    public class HomeViewModel
+    {
+        public ICommand GoToDetailsCommand => goToDetailsCommand ?? (goToDetailsCommand = new Command(GoToDetails));
+        private ICommand goToDetailsCommand;
+        private readonly INavigation navigation;
+
+        public HomeViewModel(INavigation navigation)
+        {
+            this.navigation = navigation;
+        }
+
+        private void GoToDetails()
+        {
+            navigation.PushAsync(new DetailsPage());
+        }
+    }
+}
--- src/NBPCurrencyMobile/Views/HomePage.xaml.cs
+++ src/NBPCurrencyMobile/Views/HomePage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using NBPCurrencyMobile.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,6 +16,8 @@ namespace NBPCurrencyMobile.Views
         {
             InitializeComponent();
 
+            BindingContext = new HomeViewModel(Navigation);
+
             // Example listview data
             CurrencyListView.ItemsSource = new List<object>
             {
@@ -23,6 +25,13 @@ namespace NBPCurrencyMobile.Views
                 new { CurrencyName = "dolar australijski", CurrencyCode = "AUD", CurrencyAverageExchangeRate = 2.67 },
                 new { CurrencyName = "dolar kanadyjski", CurrencyCode = "CAD", CurrencyAverageExchangeRate = 2.99 }
             };
+        }
+
+        private void CurrencyListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            HomeViewModel homeViewModel = BindingContext as HomeViewModel;
+            homeViewModel.GoToDetailsCommand.Execute(e.Item);
+            CurrencyListView.SelectedItem = null;
         }
     }
 }
