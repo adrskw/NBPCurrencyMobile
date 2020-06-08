350aedce7c8aedaeaf6e2d8e1b2e0ec118710414
adrskw <48623654+adrskw@users.noreply.github.com>
2020-06-09 00:02:38 +0200

    Initialize NBP client

--- src/NBPCurrencyMobile/App.xaml.cs
+++ src/NBPCurrencyMobile/App.xaml.cs
@@ -3,15 +3,19 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using NBPCurrencyMobile.Views;
 using System.Globalization;
+using NBPCurrencyCore;
 
 namespace NBPCurrencyMobile
 {
     public partial class App : Application
     {
+        public static NbpClient NbpClient { get; set; }
+
         public App()
         {
             InitializeComponent();
             CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pl-PL");
+            NbpClient = new NbpClient();
 
             MainPage = new TabsPage();
         }
