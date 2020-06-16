a0f982f3a84c7990cc508dad8213c35278030dab
adrskw <48623654+adrskw@users.noreply.github.com>
2020-06-17 01:23:18 +0200

    Add viewmodel and create commands for each setting

---
+++ src/NBPCurrencyMobile/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using NBPCurrencyMobile.Models;
+using NBPCurrencyMobile.Views.Controls;
+using Xamarin.Forms;
+
+namespace NBPCurrencyMobile.ViewModels
+{
+    public class SettingsViewModel : BaseViewModel
+    {
+        private List<SettingsCurrencySwitchCellControl> currencyDisplaySwitches;
+
+        public List<SettingsCurrencySwitchCellControl> CurrencyDisplaySwitches
+        {
+            get => currencyDisplaySwitches;
+            set => SetProperty(ref currencyDisplaySwitches, value);
+        }
+
+        private DateTime defaultStartDate;
+
+        public DateTime DefaultStartDate
+        {
+            get => defaultStartDate;
+            set => SetProperty(ref defaultStartDate, value);
+        }
+
+        private DateTime defaultEndDate;
+
+        public DateTime DefaultEndDate
+        {
+            get => defaultEndDate;
+            set
+            {
+                SetProperty(ref defaultEndDate, value);
+                CalculateMinimumDefaultStartDate();
+            }
+        }
+
+        private DateTime minimumDefaultStartDate;
+
+        public DateTime MinimumDefaultStartDate
+        {
+            get => minimumDefaultStartDate;
+            set => SetProperty(ref minimumDefaultStartDate, value);
+        }
+
+        public DateTime MinimumDefaultDate { get; } = new DateTime(2002, 1, 2);
+
+        public DateTime MaximumDefaultDate { get; } = DateTime.Now;
+
+        public ICommand UpdateDefaultStartDateCommand => updateDefaultStartDateCommand ?? (updateDefaultStartDateCommand = new Command<DateTime>(UpdateDefaultStartDate));
+        private ICommand updateDefaultStartDateCommand;
+
+        public ICommand UpdateDefaultEndDateCommand => updateDefaultEndDateCommand ?? (updateDefaultEndDateCommand = new Command<DateTime>(UpdateDefaultEndDate));
+        private ICommand updateDefaultEndDateCommand;
+
+        public ICommand UpdateDisplayedCurrencySettingCommand => updateDisplayedCurrencySettingCommand ?? (updateDisplayedCurrencySettingCommand = new Command<SettingsCurrencySwitchCellControl>(UpdateDisplayedCurrencySetting));
+        private ICommand updateDisplayedCurrencySettingCommand;
+
+        public SettingsViewModel()
+        {
+            GetCurrentDefaultDates();
+            GetCurrentDisplayedCurrenciesSettings();
+            CalculateMinimumDefaultStartDate();
+        }
+
+        public void GetCurrentDefaultDates()
+        {
+            SettingEntity defaultStartDate = App.Database.GetSetting("DefaultStartDate");
+            SettingEntity defaultEndDate = App.Database.GetSetting("DefaultEndDate");
+
+            DefaultStartDate = Convert.ToDateTime(defaultStartDate.Value);
+            DefaultEndDate = Convert.ToDateTime(defaultEndDate.Value);
+        }
+
+        public void GetCurrentDisplayedCurrenciesSettings()
+        {
+            List<SettingDisplayedCurrencyEntity> currencyDisplaySettings = App.Database.GetDisplayedCurrenciesSettings();
+            CurrencyDisplaySwitches = currencyDisplaySettings.Select(
+                setting => new SettingsCurrencySwitchCellControl()
+                {
+                    CurrencyName = setting.CurrencyName,
+                    CurrencyCode = setting.CurrencyCode,
+                    IsToggled = setting.IsDisplayed
+                }).ToList();
+        }
+
+        private void UpdateDefaultStartDate(DateTime date)
+        {
+            DefaultStartDate = date;
+            SettingEntity setting = new SettingEntity("DefaultStartDate", date.ToString("d"));
+            App.Database.UpdateSetting(setting);
+        }
+
+        private void UpdateDefaultEndDate(DateTime date)
+        {
+            DefaultEndDate = date;
+            SettingEntity setting = new SettingEntity("DefaultEndDate", date.ToString("d"));
+            App.Database.UpdateSetting(setting);
+        }
+
+        private void UpdateDisplayedCurrencySetting(SettingsCurrencySwitchCellControl item)
+        {
+            SettingDisplayedCurrencyEntity setting = new SettingDisplayedCurrencyEntity(item.CurrencyCode, item.CurrencyName, item.IsToggled);
+            App.Database.UpdateDisplayedCurrencySetting(setting);
+        }
+
+        private void CalculateMinimumDefaultStartDate()
+        {
+            MinimumDefaultStartDate = DefaultEndDate.AddDays(-App.NbpClient.MaximumNumberOfDaysForDownloadingData);
+        }
+    }
+}
--- src/NBPCurrencyMobile/Views/SettingsPage.xaml.cs
+++ src/NBPCurrencyMobile/Views/SettingsPage.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NBPCurrencyCore.Models;
+using NBPCurrencyMobile.Models;
+using NBPCurrencyMobile.ViewModels;
 using NBPCurrencyMobile.Views.Controls;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -13,17 +15,41 @@ namespace NBPCurrencyMobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SettingsPage : ContentPage
     {
+        public SettingsViewModel SettingsViewModel { get; }
+
         public SettingsPage()
         {
             InitializeComponent();
+
+            SettingsViewModel = new SettingsViewModel();
+            BindingContext = SettingsViewModel;
         }
 
         private void StartDateSetting_OnDateSelected(object sender, DateChangedEventArgs e)
         {
+            SettingsViewModel.UpdateDefaultStartDateCommand.Execute(e.NewDate);
         }
 
         private void EndDateSetting_OnDateSelected(object sender, DateChangedEventArgs e)
         {
+            SettingsViewModel.UpdateDefaultEndDateCommand.Execute(e.NewDate);
+        }
+
+        private void DisplayedCurrenciesSettings_SwitchToggled(object sender, ToggledEventArgs e)
+        {
+            SettingsCurrencySwitchCellControl item = sender as SettingsCurrencySwitchCellControl;
+
+            SettingsViewModel.UpdateDisplayedCurrencySettingCommand.Execute(item);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (SettingsViewModel.CurrencyDisplaySwitches.Count == 0)
+            {
+                SettingsViewModel.GetCurrentDisplayedCurrenciesSettings();
+            }
         }
     }
 }
