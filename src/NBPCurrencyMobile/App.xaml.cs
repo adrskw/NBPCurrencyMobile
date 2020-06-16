0de7708ae7a80d0be37013de94cba46b1118161d
adrskw <48623654+adrskw@users.noreply.github.com>
2020-06-17 00:04:37 +0200

    Initialize database and implement IDisposable interface

--- src/NBPCurrencyMobile/App.xaml.cs
+++ src/NBPCurrencyMobile/App.xaml.cs
@@ -4,32 +4,53 @@ using Xamarin.Forms.Xaml;
 using NBPCurrencyMobile.Views;
 using System.Globalization;
 using NBPCurrencyCore;
+using NBPCurrencyMobile.Models;
 
 namespace NBPCurrencyMobile
 {
     public partial class App : Application
     {
         public static NbpClient NbpClient { get; set; }
+        public static DatabaseHelper Database { get; private set; }
 
         public App()
         {
             InitializeComponent();
+            InitializeApp();
+        }
+
+        private void InitializeApp()
+        {
             CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pl-PL");
             NbpClient = new NbpClient();
+            InitializeDatabase();
 
             MainPage = new TabsPage();
         }
 
+        private void InitializeDatabase()
+        {
+            if (Database == null)
+            {
+                Database = new DatabaseHelper();
+                Database.Initialize();
+            }
+        }
+
         protected override void OnStart()
         {
+            InitializeDatabase();
         }
 
         protected override void OnSleep()
         {
+            Database.Dispose();
+            Database = null;
         }
 
         protected override void OnResume()
         {
+            InitializeDatabase();
         }
     }
 }
