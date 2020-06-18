9443a761a868b0042cd10bfe02025f6e83820a82
adrskw <48623654+adrskw@users.noreply.github.com>
2020-06-18 22:45:26 +0200

    Create entity to store exchange rates in database

---
+++ src/NBPCurrencyMobile/Models/TableExchangeRateEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NBPCurrencyCore.Models;
+using SQLite;
+
+namespace NBPCurrencyMobile.Models
+{
+    public class TableExchangeRateEntity
+    {
+        [PrimaryKey]
+        public string CurrencyCode { get; set; }
+
+        [NotNull]
+        public string CurrencyName { get; set; }
+
+        [NotNull]
+        public decimal Bid { get; set; }
+
+        [NotNull]
+        public decimal Ask { get; set; }
+
+        public TableExchangeRateEntity()
+        {
+        }
+
+        public TableExchangeRateEntity(TableExchangeRate tableExchangeRate)
+        {
+            CurrencyCode = tableExchangeRate.CurrencyCode;
+            CurrencyName = tableExchangeRate.CurrencyName;
+            Bid = tableExchangeRate.Bid;
+            Ask = tableExchangeRate.Ask;
+        }
+    }
+}
